f8605e59812fb692d3c3e16bd09c6bb64b83f9aa
fullstack <cristian@teral.com.co>
2023-10-20 09:56:33 -0500

    [add] se agrega el puerto

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import app from "./src/app";
+import { firebaseConfig } from "./src/settings";
+console.log(firebaseConfig);
 
 const port = 4000;
-app.listen(port, () => {
+app.listen(process.env.port || port, () => {
   console.log(`Server is running on port ${port}`);
 });
